5c2318104541586967397631a4ee9ff28467b984
shaoshuai <stanfilchanger@gmail.com>
2020-01-12 23:30:49 +0800

    feat(wallpaper): 多线程下载

--- wallpaper/wallpaper.py
+++ wallpaper/wallpaper.py
@@ -1,25 +1,38 @@
 import requests
 import re
 import time
+import threading
+import math
+
+CNT = 0
+# 每个线程下载图片数
+CAPACITY = 10
+
+class httpThread (threading.Thread):
+    def __init__(self, threadID, threadName, httpList):
+        threading.Thread.__init__(self)
+        self.id = threadID
+        self.name = threadName
+        self.list = httpList
+    
+    def run(self):
+        for image in self.list:
+            downloadImg(image, self.name)
+        
 
 
 def main(): 
-    cnt = 0
+    images = getAllImageUrl()
+    l = len(images)
+    threadNum = math.ceil(l / CAPACITY)
+    for i in range(threadNum):
+        if (i + 1) * CAPACITY <= l:
+            imageList = images[i * CAPACITY : (i + 1) * CAPACITY]
+        else:
+            imageList = images[i * CAPACITY : l]
+        httpThread(i+1, 'thread_' + str(i+1), imageList)
 
-    for i in range(20):
-        homePage = 'https://vlad.studio/wallpapers/?sort=newest&filter=all&page=' + str(i)
-        text = requests.get(homePage).text
-
-        pattern = re.compile('src="(https://files.vlad.studio/sequoia/.*?\.jpg)"')
-        images = pattern.findall(text)
-        images = list(map(transformPx, images))
-
-        for image in images:
-            content = requests.get(image).content
-            with open('./wallpapers/' + str(cnt) + '.jpg', 'wb') as f:
-                f.write(content)
-            print(cnt+1)
-            cnt += 1
+        
 
 
 def transformPx(url):
@@ -27,7 +40,33 @@ def transformPx(url):
     return '/'.join(arr[:-1]) + '/1920x1080.jpg'
 
 
+def getAllImageUrl(): 
+    imgArr = []
+    for i in range(50):
+        homePage = 'https://vlad.studio/wallpapers/?sort=newest&filter=all&page=' + str(i)
+        text = requests.get(homePage).text
+
+        pattern = re.compile('src="(https://files.vlad.studio/sequoia/.*?\.jpg)"')
+        images = pattern.findall(text)
+        images = list(map(transformPx, images))
+        imgArr.extend(images)
+    return imgArr
+
+
+def downloadImg(image, threadName):
+    global CNT
+    CNT += 1
+    content = requests.get(image).content
+    name = image.split('/')[-3] + '.jpg'
+    with open('./wallpapers/' + name, 'wb') as f:
+        f.write(content)
+    print(CNT, ':', threadName, ':', name)
+
+
+def doneLog():
+    print('Done')
+    print('Time Cost:')
+    print(time.process_time())
+
+
 main()
-print('Done')
-print('Time Cost:')
-print(time.process_time())
